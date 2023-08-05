e5729c26e68163af193aa56e185f39ee3b04e275
DKazaza <dk.1995@mail.ru>
2023-08-05 16:36:42 +0300

    новый поиск

--- src/scraping_service/urls.py
+++ src/scraping_service/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from scraping.views import home_view
 
 urlpatterns = [
     path('nimda/', admin.site.urls),
-    path('home/', home),
+    path('home/', home_view),
 ]
